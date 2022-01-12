1fc6928c48e96ea5988706f3f2984d2de5929e43
Paul BRIE <aelythe@gmail.com>
2022-01-12 16:13:15 +0200

    yes!

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import { AppStateProvider } from './store' 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { AppStateProvider } from "./store";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import './teleporthq/style.module.css'
-import Home from './teleporthq/pages/home'
+import "./teleporthq/style.module.css";
+import Home from "./teleporthq/pages/home";
+
+// hello new info here :)
 
 const App = () => {
   return (
@@ -13,7 +15,7 @@ const App = () => {
         <Route exact component={Home} path="/" />
       </AppStateProvider>
     </Router>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById("app"));
